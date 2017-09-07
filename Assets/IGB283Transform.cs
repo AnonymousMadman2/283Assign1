4e9b314c74758c1622393ce1a82adfb247ca567d
Gaxsun <Razgriz248@gmail.com>
2017-09-07 15:54:45 +1000

    technical win

---
+++ Assets/IGB283Transform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IGB283Transform : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public static Vector3[] rotate(Vector3[] vertices, float angle) {
+
+        // Get the rotation matrix
+        Matrix3x3 R = rotateMatrix(angle * Time.deltaTime);
+
+        for (int i = 0; i < vertices.Length; i++) {
+            vertices[i] = R.MultiplyPoint(vertices[i]);
+        }
+        return vertices;
+    }
+
+    public static Vector3[] translate(Vector3[] vertices, Vector3 point) {
+
+        for (int i = 0; i < vertices.Length; i++) {
+            vertices[i] += point;
+        }
+        return vertices;
+    }
+
+    static Matrix3x3 rotateMatrix(float angle) {
+
+        Matrix3x3 matrix = new Matrix3x3();
+
+        matrix.SetRow(0, new Vector3(Mathf.Cos(angle), -Mathf.Sin(angle), 0.0f));
+        matrix.SetRow(1, new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0.0f));
+        matrix.SetRow(2, new Vector3(0.0f, 0.0f, 1.0f));
+
+        return matrix;
+    }
+}
---
+++ Assets/RenderTriangle.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTriangle : MonoBehaviour {
+
+    public float angle = 1;
+    public Vector3 point;
+    public static Material material;
+    public static Mesh mesh;
+    Vector3[] triangleVertecies = new Vector3[] { new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(1, 1, 0),};
+
+    // Use this for initialization
+    void Start () {
+        // Add a MeshFilter and MeshRenderer to the Empty GameObject
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+
+        // Get the Mesh from the MeshFilter
+        mesh = GetComponent<MeshFilter>().mesh;
+
+        // Set the material to the material we have selected
+        GetComponent<MeshRenderer>().material = material;
+
+        // Clear all vertex and index data from the mesh
+        mesh.Clear();
+
+        // Create a triangle with points at (0, 0, 0), (0, 1, 0) and(1, 1, 0)
+        mesh.vertices = triangleVertecies;
+
+        // Set the colour of the triangle
+        mesh.colors = new Color[] { new Color(0.8f, 0.3f, 0.3f, 1.0f), new Color(0.8f, 0.3f, 0.3f, 1.0f), new Color(0.8f, 0.3f, 0.3f, 1.0f)};
+
+        // Set vertex indicies
+        mesh.triangles = new int[] { 0, 1, 2};
+        }
+	
+	// Update is called once per frame
+	void Update () {
+        mesh.vertices = IGB283Transform.translate(mesh.vertices, point);
+        
+        mesh.vertices = IGB283Transform.rotate(mesh.vertices, angle);
+        
+        mesh.RecalculateBounds();
+    }
+}
